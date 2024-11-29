e90e0614c44f4b504a61a7b2b998c2a3cb3cacac
Iuri Trevizani <iurirosa250@gmail.com>
2024-11-29 06:28:03 -0300

    Aplicando o Data Annotations

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa3d5564d147a8360b0071ca84bcd39a56a58bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9799d3c04d2b26add81c16f7f823ec63e384e5a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
