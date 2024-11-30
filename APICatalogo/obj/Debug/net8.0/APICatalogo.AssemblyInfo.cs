baf25c750871b6ab54dcb8a16e82b8f9f420779a
Iuri Trevizani <iurirosa250@gmail.com>
2024-11-30 16:45:43 -0300

    ProdutosController - GET e Populando a tabela Produtos

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90e0614c44f4b504a61a7b2b998c2a3cb3cacac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3582a054459d68e8f85a28273711449916306ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
