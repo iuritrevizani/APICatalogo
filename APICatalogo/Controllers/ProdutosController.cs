31a535ba4a0752af6639462a3fb8e02a51374b84
Iuri Trevizani <iurirosa250@gmail.com>
2024-12-02 06:23:40 -0300

    ProdutosController - GET por ID

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -7,23 +7,38 @@ namespace APICatalogo.Controllers
     [Route("[controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
-    {
-     private readonly AppDbContext _context;
+        {
+            private readonly AppDbContext _context;
         
-     public ProdutosController(AppDbContext context)
-     {
-         _context = context;
-     }
-
-     [HttpGet]
-     public ActionResult<IEnumerable<Produto>> Get()
+            public ProdutosController(AppDbContext context)
         {
-        var produtos = _context.Produtos.ToList();
-        if (produtos is null)
-        {
-        return NotFound("Produtos não encontrados...");
+            _context = context;
         }
-        return produtos;
-     }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+            return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult <Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+
+
+
+
+
     }
 }
