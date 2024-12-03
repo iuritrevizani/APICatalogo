9e65dc9f72245b61e8861dfd3d7af80944e918a2
Iuri Trevizani <iurirosa250@gmail.com>
2024-12-03 20:00:42 -0300

    ProdutosController - DELETE

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bec344e00dbfbf85465ce583e7a36861329afa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57629d9bfbd2a46f9b58978f86c706077242c888")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
